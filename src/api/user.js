af8571930a75466b14311ff2018bc6acfbd40434
wangqiwei0504 <1046005995@qq.com>
2022-06-26 18:04:31 +0800

    完成登录功能

---
+++ src/api/user.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+export const getSmsCode = (mobile) => request({
+  url: `/sms/codes/${mobile}`
+})
+
+/**
+ *
+ * @param {mobile,code} param0
+ * @returns promise
+ */
+export const login = ({ mobile, code }) => request({
+  method: 'POST',
+  url: '/authorizations',
+  data: { mobile, code }
+})
